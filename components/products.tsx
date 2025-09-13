3b6a42c72a5cfa0ba3d6deed5d4c3034f0730fef
- Z e r o <kimianalconera39@gmail.com>
2025-09-14 00:40:59 +0800

    update: link to products page

--- components/products.tsx
+++ components/products.tsx
@@ -30,8 +30,6 @@ interface ProductProps {
 }
 
 export default function Products({ products }: ProductProps) {
-  console.log('Shopify products:', products)
-
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl font-bold tracking-tight text-foreground">Our Products</h2>
