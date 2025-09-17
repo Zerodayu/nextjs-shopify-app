2c3c318b58c3da1ba949ab147950ab501f736d04
- Z e r o <kimianalconera39@gmail.com>
2025-09-17 21:55:45 +0800

    update: app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Next.js Shopify App",
+  title: "MirroriFy - AI Virtual Try-On",
   description: "AI Virtual Try-On Shopify App",
 };
 
