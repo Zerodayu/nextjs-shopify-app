600332ad35653e27f6313b6cd9a55e70e574faab
- Z e r o <kimianalconera39@gmail.com>
2025-09-17 21:38:55 +0800

    update: hero section description texts

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -12,7 +12,7 @@ import { Info, ArrowRight, SquareArrowOutUpRight } from "lucide-react";
 export default function HeroSection() {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center">
-        <div>
+        <div className="max-w-6xl text-center flex flex-col gap-2">
             <Announcement>
                 <AnnouncementTag>Demo</AnnouncementTag>
                 <AnnouncementTitle>
@@ -21,6 +21,8 @@ export default function HeroSection() {
                 </AnnouncementTitle>
             </Announcement>
             <h1 className="from-primary/50 via-foreground/85 to-primary/20 bg-gradient-to-tl bg-clip-text text-center tracking-tighter text-balance text-transparent text-5xl md:text-8xl py-4">AI Virtual Try-On Shopify App</h1>
+
+            <p className="text-muted-foreground text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae dicta nulla qui aperiam iusto ut ea aliquam, minima voluptatem distinctio facere illum numquam expedita sapiente! Dolores omnis eaque sed temporibus.</p>
         </div>
 
         <div className="flex gap-4 mt-8">
